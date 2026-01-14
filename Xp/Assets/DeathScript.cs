e8704c374d64922271f6a21e2b2076392e0a894b
BorhanMousavi <seyed.mousaviminabi@elev.ga.lbs.se>
2026-01-14 11:40:35 +0100

    fuh

--- Xp/Assets/DeathScript.cs
+++ Xp/Assets/DeathScript.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
 public class KillPlayer : MonoBehaviour
+
 {
+    [SerializeField] GameObject Player;
+    [SerializeField] GameObject Player2;
+
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            PlayerRespawn respawn = other.GetComponent<PlayerRespawn>();
+            Player.transform.position = new Vector2(-42, -7);
+            Player2.transform.position = new Vector2(-42, -7)
+; PlayerRespawn respawn = other.GetComponent<PlayerRespawn>();
             if (respawn != null)
             {
                 respawn.Die();
             }
+
         }
     }
+
 }
+
