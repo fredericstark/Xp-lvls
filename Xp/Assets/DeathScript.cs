52b15e0104f37c0e04b3839db80b61f68041652d
adrianeklund <adrian.eklund@elev.ga.lbs.se>
2026-01-16 13:25:09 +0100

    deathscript fix

--- Xp/Assets/DeathScript.cs
+++ Xp/Assets/DeathScript.cs
@@ -1,20 +1,31 @@
 using UnityEngine;
 
-public class DeathObject : MonoBehaviour
+
+public class RespawnTile2D : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private Vector2 respawnBasePosition = new Vector2(-42f, -7f);
+    private float playerOffset = 1.5f;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-
-        if (collision.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            PlayerRespawn respawn =
-                collision.gameObject.GetComponent<PlayerRespawn>();
+            Respawn(other, respawnBasePosition + Vector2.left * playerOffset);
+        }
+        else if (other.CompareTag("Player2"))
+        {
+            Respawn(other, respawnBasePosition + Vector2.right * playerOffset);
+        }
+    }
 
-            if (respawn != null)
-            {
-                respawn.Die();
-            }
+    private void Respawn(Collider2D player, Vector2 position)
+    {
+        player.transform.position = position;
+
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector2.zero;
         }
     }
 }
-
